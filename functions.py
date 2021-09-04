b53827948a0fc710130e86920ed30b6bacb518dd
ElMajdoubraed <raed.elmajdoub@isticbc.org>
2021-09-04 05:27:36 +0200

    functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+'''
+def function_name():
+    code
+
+- call function
+ function_name()
+
+'''
+
+def writing_program():
+    print("program")
+
+def add(x,y):
+    return (x+y)
+
+def sub(x,y):
+    return (x-y)
+
+#writing_program()
